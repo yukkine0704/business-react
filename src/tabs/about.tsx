47936a2dc1e8ede6b7c6d35ab146c8b3b9dc1b50
Kevin <lubbok0704@gmail.com>
2024-10-12 14:02:59 -0400

    style: :lipstick: Added colors to the UI elemets

--- src/tabs/about.tsx
+++ src/tabs/about.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Text } from 'react-native-paper'
+import { StyleSheet } from 'react-native';
+import { Text, useTheme } from 'react-native-paper'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function About() {
+    const theme = useTheme();
   return (
-    <SafeAreaView>
+    <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.surface}]}>
         <Text>About</Text>
     </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1, // Asegura que SafeAreaView ocupe toda la pantalla
+      justifyContent: 'center', // Centra el contenido verticalmente (opcional)
+      alignItems: 'center', // Centra el contenido horizontalmente (opcional)
+    },
+  });
--- src/tabs/home.tsx
+++ src/tabs/home.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Text } from 'react-native-paper'
+import React from 'react';
+import { Text, useTheme } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
-  return (
-    <SafeAreaView>
-        <Text>Home</Text>
-    </SafeAreaView>
-    
-  )
-}
+    const theme = useTheme();
+    return (
+        <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.surface}]}>
+          <Text>Home</Text>
+        </SafeAreaView>
+      );
+    }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Asegura que SafeAreaView ocupe toda la pantalla
+    justifyContent: 'center', // Centra el contenido verticalmente (opcional)
+    alignItems: 'center', // Centra el contenido horizontalmente (opcional)
+  },
+});
