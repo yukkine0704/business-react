d0cd981a149ec86f46b41feae1b27bcba8c7a955
Kevin <lubbok0704@gmail.com>
2024-10-12 12:06:25 -0400

    feat: :sparkles: Added bottom tab navigator
    Created the main navigation of the app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './src/rootNavigator';
 
 export default function App() {
   return <NavigationContainer>
-    Hello
+    <Navigator/>
   </NavigationContainer>
 }
 
--- src/rootNavigator.jsx
+++ src/rootNavigator.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
 import { createMaterialBottomTabNavigator } from 'react-native-paper/lib/typescript/react-navigation'
+import home from './tabs/home';
+import about from './tabs/about';
 
-const Tabs = createMaterialBottomTabNavigator();
-const Stack = createSta
 
-export default function rootNavigator() {
+
+const Stack = createStackNavigator();
+const Tabs = createMaterialBottomTabNavigator(); // Mueve esta línea antes de usar Tabs
+
+const TabNavigator = () => {
   return (
-    
-  )
-}
-x   
+    <Tabs.Navigator>
+      <Tabs.Screen name='Home' component={home} />
+      <Tabs.Screen name='About' component={about} />
+    </Tabs.Navigator>
+  );
+};
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={TabNavigator} />
+    </Stack.Navigator>
+  );
+};
+
+export default Navigator;
