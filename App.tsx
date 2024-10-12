d874e3cbf03f01f39c36de40d863406859d2b825
Kevin <lubbok0704@gmail.com>
2024-10-12 13:48:16 -0400

    docs: :memo: Added DeepMerge for theming options

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './src/rootNavigator';
+import { useMaterial3Theme } from '@pchmn/expo-material3-theme';
+import { useMemo } from 'react';
+import { useColorScheme } from 'react-native';
+import { MD3DarkTheme, MD3LightTheme, Provider as PaperProvider } from 'react-native-paper';
 
 
 export default function App() {
-  return <NavigationContainer>
+
+  const colorScheme = useColorScheme();
+  const {theme} = useMaterial3Theme({fallbackSourceColor: '#FF6214'})
+
+  const paperTheme = useMemo(
+    () =>
+      colorScheme === 'dark' ? { ...MD3DarkTheme, colors: theme.dark } : { ...MD3LightTheme, colors: theme.light },
+    [colorScheme, theme]
+  );
+
+  return (
+  <PaperProvider theme={paperTheme}>
+  <NavigationContainer>
     <Navigator/>
   </NavigationContainer>
+  </PaperProvider>
+  );
 }
 
 
